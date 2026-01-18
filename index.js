f175aec4b2074a80c6a0a0c149b800f15a1e7bfa
Rubina Akther <rubinaakther3454@gmail.com>
2026-01-18 22:57:41 +0600

    Contact form (schema, router and logics)

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 const connectDB = require('./utils/db');
 const authRouter = require('./routes/auth.routes.js');
+const contactRouter = require('./routes/contact.router.js')
 const errorMiddleware = require('./middlewares/error.middleware.js');
 
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+app.use('/api/form', contactRouter)
 
 app.use(errorMiddleware);
 
