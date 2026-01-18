bbb7b93194cba006b79c1ca31500f34a9e7b287c
shajid027 <shajidhasan013@gmail.com>
2026-01-18 11:59:33 +0600

    hash password

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -24,6 +24,22 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// hash password
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (!user.isModified('password')) {
+    next();
+  }
+
+  try {
+    const saltRound = await bcrypt.genSalt(10);
+    const hash_password = await bcrypt.hash(user.password, saltRound);
+    user.password = hash_password;
+  } catch (error) {
+    next(error);
+  }
+});
 
 const User = new mongoose.model('user', userSchema);
 module.exports = User;
